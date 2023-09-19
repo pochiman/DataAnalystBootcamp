08ab3d1e26af846dd20ea846f5870200e29c605e
pochiman <pochiman@yahoo.com>
2023-09-18 20:44:01 -0400

    initial commit

---
+++ 02_SQL_Intermediate/03_Case_Statement_Use_Cases.sql
@@ -0,0 +1,72 @@
+/*
+Case Statement
+*/
+
+
+
+SELECT FirstName, LastName, Age
+FROM SQLTutorial.dbo.EmployeeDemographics
+WHERE Age is NOT NULL
+ORDER BY Age
+
+
+
+SELECT FirstName, LastName, Age,
+CASE
+    WHEN Age > 30 THEN 'Old'
+    ELSE 'Young'
+END
+FROM SQLTutorial.dbo.EmployeeDemographics
+WHERE Age is NOT NULL
+ORDER BY Age
+
+
+
+SELECT FirstName, LastName, Age,
+CASE
+    WHEN Age > 30 THEN 'Old'
+    WHEN Age BETWEEN 27 AND 30 THEN 'Young'
+    ELSE 'Baby'
+END
+FROM SQLTutorial.dbo.EmployeeDemographics
+WHERE Age is NOT NULL
+ORDER BY Age
+
+
+
+SELECT FirstName, LastName, Age,
+CASE
+    WHEN Age = 38 THEN 'Stanley'
+    WHEN Age > 30 THEN 'Old'
+    ELSE 'Baby'
+END
+FROM SQLTutorial.dbo.EmployeeDemographics
+WHERE Age is NOT NULL
+ORDER BY Age
+
+
+
+SELECT *
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+    ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+
+
+SELECT FirstName, LastName, JobTitle, Salary
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+    ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+
+
+SELECT FirstName, LastName, JobTitle, Salary,
+CASE
+    WHEN JobTitle = 'Salesman' THEN Salary + (Salary * .10)
+    WHEN JobTitle = 'Accountant' THEN Salary + (Salary * .05)
+    WHEN JobTitle = 'HR' THEN Salary + (Salary * .000001)
+    ELSE Salary + (Salary * .03)
+END AS SalaryAfterRaise
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+    ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
---
+++ 02_SQL_Intermediate/04_Having_Clause.sql
@@ -0,0 +1,39 @@
+/*
+Having Clause
+*/
+
+
+
+SELECT JobTitle, COUNT(JobTitle)
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+    ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+
+
+
+SELECT JobTitle, COUNT(JobTitle)
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+    ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+HAVING COUNT(JobTitle) > 1
+
+
+
+SELECT JobTitle, AVG(Salary)
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+    ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+ORDER BY AVG(Salary)
+
+
+
+SELECT JobTitle, AVG(Salary)
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+    ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+HAVING AVG(Salary) > 45000
+ORDER BY AVG(Salary)
